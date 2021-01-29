44fcae8f9110d5335d90a2b190112c85956eb665
lutao <1519094399@qq.com>
2021-01-29 15:30:10 +0800

    提交首页

--- assets/js/commonApi.js
+++ assets/js/commonApi.js
@@ -2,6 +2,11 @@ axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net'
 
 // 添加请求拦截器
 axios.interceptors.request.use(function(config) {
+    const token = localStorage.getItem('token') || ''
+
+    if (config.url.indexOf('/my') == 0) {
+        config.headers.Authorization = token
+    }
     // 在发送请求之前做些什么
     return config;
 }, function(error) {
@@ -12,6 +17,19 @@ axios.interceptors.request.use(function(config) {
 // 添加响应拦截器
 axios.interceptors.response.use(function(response) {
     // 对响应数据做点什么
+
+    const { message, status } = response.data
+    if (message == '身份认证失败！' && status == 1) {
+        localStorage.removeItem('token')
+        location.href = './login.html'
+    }
+
+
+
+
+
+
+
     return response.data;
 }, function(error) {
     // 对响应错误做点什么
